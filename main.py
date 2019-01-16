00e303effa4360c9ea1a10cc8489e0bd2df5991a
Salatiel <jesussalatiel21@gmail.com>
2019-01-16 00:59:10 -0600

    Cambios

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-"""
+
 import Vistas.database as db
 db.decodeKnown()
 db.decodeAdministration()
 import CNN.Recognition as rec
-"""
 
-import Vistas.logica_administrador as admin
 
-admin.startAdmin()
+#import Vistas.logica_administrador as admin
+
+#admin.startAdmin()
